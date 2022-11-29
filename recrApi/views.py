0ef4d44c5ddb344fe60e16d42d69c28e79abd027
SergeySud <ln251705@gmail.com>
2022-11-29 11:50:46 +0300

    fix

--- recrApi/views.py
+++ recrApi/views.py
@@ -12,15 +12,12 @@ def test(request):
 # Create your views here.
 class BasicView(View):
     def get(self, request, name, message, *args, **kwargs):
-        print(request)
-        print(request.get_full_path())
         return HttpResponse(f"Basic View:\nHello, {name}! {message}!", content_type="text/plain")
 
 
 class AnchoredView(View):
     def get(self, request, *args, **kwargs):
         data = request.get_full_path()
-        print(data)
         regexp = re.compile(r'^/\?name=(.*)&message=(.*)')
         if regexp.search(data):
             name_and_message = re.search('^/\?name=(.*)&message=(.*)', data)
